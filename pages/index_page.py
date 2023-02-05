62a8a1f2c5d32e3c28518e1fa60d508e4604a996
Ilia Stepushchenko <ilia.stepushchenko@gmail.com>
2023-02-05 22:24:43 +0100

    update autotest

--- pages/index_page.py
+++ pages/index_page.py
@@ -4,9 +4,13 @@ import config
 
 class IndexPage:
     _BUTTON_GOOGLE_SEARCH = "//div[@class='FPdoLc lJ9FBc']//input[@name='btnK']"
+    _LINK_ENGLISH_LANG = "//a[contains(text(), 'English')]"
 
     def open_index_page(self, page: Page) -> None:  # noqa
         page.goto(config.url.DOMAIN)
 
+    def press_link_english_lang(self, page: Page):
+        page.locator(self._LINK_ENGLISH_LANG).click()
+
     def get_text_from_google_search_button(self, page: Page) -> None:
-        return page.locator(self._BUTTON_GOOGLE_SEARCH).text_content()
+        return page.locator(self._BUTTON_GOOGLE_SEARCH).get_attribute('value')
--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -1,10 +1,11 @@
 import pytest
 import pages
-import time
 
 
 class TestFooter:
 
     def test_user_should_be_able_to_open_popup_select_subscription_plan(self, page):
         pages.index_page.open_index_page(page)
-        time.sleep(10)
+        pages.index_page.press_link_english_lang(page)
+        actual_result = pages.index_page.get_text_from_google_search_button(page)
+        assert actual_result == 'Google Search', 'Google Search button text is not correct'
