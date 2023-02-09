a71cce1e26be2c9cbda4ef2b1c58180815f6db77
Ilia Stepushchenko <ilia.stepushchenko@gmail.com>
2023-02-09 19:41:24 +0100

    update case title

--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -4,7 +4,7 @@ import pages
 
 class TestFooter:
 
-    def test_user_should_be_able_to_open_popup_select_subscription_plan(self, page):
+    def test_main_search_button_must_have_the_text_google_search(self, page):
         pages.index_page.open_index_page(page)
         pages.index_page.press_link_english_lang(page)
         actual_result = pages.index_page.get_text_from_google_search_button(page)
